e2a295582c07911e2027428994ad93eb06924317
Jeremie Smaga <jeremie@smaga.ch>
2024-04-01 21:41:06 +0800

    Enhances tests

--- tests/test_correl.py
+++ tests/test_correl.py
@@ -1,12 +1,43 @@
 import pytest
 from src.correl import CorrelationMatrix
 
-def query_non_existing_same():
+
+def test_non_existing_same_key():
 
     rho = CorrelationMatrix()
-    x = rho[ "A", "A" ]
-
-def test_answer():
-
     with pytest.raises( IndexError ):
-        query_non_existing_same()
+        #Should not be allowd
+        x = rho[ "A", "A" ]
+        
+    rho[ "A", "A" ] = 1.0
+    
+    pass
+
+def test_set_key_pair():
+    
+    rho = CorrelationMatrix()
+    
+    val = 0.5   
+    
+    rho[ "A", "B" ] = 0.5
+    
+    assert rho[ "B", "A" ] == val
+    
+    val2 = -0.2
+    
+    rho[ "B", "A" ] = val2
+    
+    assert rho[ "A", "B" ] == val2
+
+
+def test_set_key_pair_invalid():
+    
+    rho = CorrelationMatrix()
+    
+    with pytest.raises( ValueError ):
+        rho[ "A", "B" ] = 1.1
+        
+    with pytest.raises( ValueError ):
+        rho[ "A", "B" ] = -1.1
+    
+    
